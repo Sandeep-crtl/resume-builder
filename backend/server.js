5f58913dd2842c5fa9a237cc999d6af73c008a2f
Bhukya Sandeep <nayaksandeep424@gmail.com>
2025-07-29 16:54:08 +0530

    Remove resumeRoutes usage from server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/resumes', resumeRoutes);
+
 
 // MongoDB Connection
 mongoose.connect(MONGODB_URI)
